bc94f3af9f8f04a85cb5703a9035863a706a7eec
Daniel Williams <dwilliams@port8080.net>
2025-12-10 09:39:03 -0700

    Adding some tests.

--- tests/vector3_tests.cpp
+++ tests/vector3_tests.cpp
@@ -17,3 +17,39 @@ TEST(Vector3, all_components_ctor) {
     ASSERT_EQ(vec.e[1], 2.0);
     ASSERT_EQ(vec.e[2], 3.0);
 }
+
+TEST(Vector3, minus_operator) {
+    const kdrt::renderer::Vector3 vec = kdrt::renderer::Vector3{1, 2, 3};
+    const kdrt::renderer::Vector3 vec2 = -vec;
+
+    ASSERT_EQ(vec.e[0], 1.0);
+    ASSERT_EQ(vec.e[1], 2.0);
+    ASSERT_EQ(vec.e[2], 3.0);
+
+    ASSERT_EQ(vec2.e[0], -1.0);
+    ASSERT_EQ(vec2.e[1], -2.0);
+    ASSERT_EQ(vec2.e[2], -3.0);
+}
+
+TEST(Vector3, plus_equals_operator) {
+    const kdrt::renderer::Vector3 vec = kdrt::renderer::Vector3{1, 2, 3};
+    kdrt::renderer::Vector3 vec2 = kdrt::renderer::Vector3{3, 2, 1};
+    vec2 += vec;
+
+    ASSERT_EQ(vec.e[0], 1.0);
+    ASSERT_EQ(vec.e[1], 2.0);
+    ASSERT_EQ(vec.e[2], 3.0);
+
+    ASSERT_EQ(vec2.e[0], 4.0);
+    ASSERT_EQ(vec2.e[1], 4.0);
+    ASSERT_EQ(vec2.e[2], 4.0);
+}
+
+TEST(Vector3, star_equals_operator) {
+    kdrt::renderer::Vector3 vec = kdrt::renderer::Vector3{1, 2, 3};
+    vec *= 3;
+
+    ASSERT_EQ(vec.e[0], 3.0);
+    ASSERT_EQ(vec.e[1], 6.0);
+    ASSERT_EQ(vec.e[2], 9.0);
+}
