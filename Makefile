7c88fb1e901376021074a4bb709881e8ee22a7df
Daniel Williams <dwilliams@port8080.net>
2025-12-09 17:35:47 -0700

    First pass at code coverage for tests using clang.

--- Makefile
+++ Makefile
@@ -9,5 +9,9 @@ build:
 tests: build
 	./build/Debug/tests/unit_tests --gtest_color=yes
 
+coverage: tests
+	llvm-profdata merge -sparse default.profraw -o default.profdata
+	llvm-cov report ./build/Debug/tests/unit_tests -instr-profile=default.profdata
+
 clean:
 	rm -rf ./build
--- tests/vector3_tests.cpp
+++ tests/vector3_tests.cpp
@@ -9,3 +9,11 @@ TEST(Vector3, empty_ctor) {
     ASSERT_EQ(vec.e[1], 0.0);
     ASSERT_EQ(vec.e[2], 0.0);
 }
+
+TEST(Vector3, all_components_ctor) {
+    const kdrt::renderer::Vector3 vec = kdrt::renderer::Vector3{1, 2, 3};
+
+    ASSERT_EQ(vec.e[0], 1.0);
+    ASSERT_EQ(vec.e[1], 2.0);
+    ASSERT_EQ(vec.e[2], 3.0);
+}
